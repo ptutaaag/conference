05ea9ec5df4342282c6a2a376f394e7b3db769c9
ptutaaag <ptut.aaag@gmail.com>
2014-01-17 16:34:56 +0100

    ListeDesEvenements.php  + PageEvenement.php

---
+++ PageEvenement.php
@@ -0,0 +1,27 @@
+<?php
+include_once("Evenement.php");
+/**
+ * Created by PhpStorm.
+ * User: Geoffrey
+ * Date: 17/01/14
+ * Time: 14:32
+ */
+
+
+$connection = mysqli_connect($_SESSION['db_host'],$_SESSION['db_user'],$_SESSION['db_password'],$_SESSION['db_dbname']);
+$req = "SELECT * FROM evenement WHERE idEvenement=".$_GET['id'];
+
+$res = mysqli_query($connection,$req) or die ("Erreur dans la recuperation de l'evenement");
+
+while ($ligne = mysqli_fetch_object($res))
+{
+    echo "<div class=contenuEvenement>
+    <span>".$ligne->NomEvent."</span><br/>
+    <span>".$ligne->Description."</span><br/>
+    <span>".$ligne->HeureDebut."</span><br/>
+    <span>".$ligne->HeureFin."</span><br/>
+    <span>".$ligne->Lieu."</span><br/>
+    </div>";
+}
+
+?>
