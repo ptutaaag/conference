aab2feb92983d104d0823d9d17367437cb0729dc
Adelinegen <missdi.dine@hotmail.fr>
2014-01-20 16:58:46 +0100

    Messagerie upload

---
+++ Supprimer.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['login'])) {
+header ('Location: index.php');
+exit();
+}
+
+if (!isset($_GET['idMessage']) || empty($_GET['idMessage'])) {
+header ('Location: MessageUser.php');
+exit();
+}
+else {
+	$db = new mysqli($_SESSION['db_host'], $_SESSION['db_user'], $_SESSION['db_password'], $_SESSION['db_dbname']);
+
+	$sql = 'DELETE FROM message WHERE id_destinataire="'.$_SESSION['iduser'].'" AND idMessage="'.$_GET['idMessage'].'"';
+	$req = mysqli_query($db,$sql) or die(mysqli_connect_error());
+
+	mysqli_close($db);
+
+header ('Location: MessageUser.php');
+exit();
+}
+?>
