fa626ebd483efa07c5d8222cf3e1e754ba8a566b
BomberCraft <bomber.minecraft@laposte.net>
2014-01-24 15:15:27 +0100

    init master

--- MVC/PTUT_MVC/Controler/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Geoffrey
- * Date: 23/01/14
- * Time: 10:10
- */
-
-include_once("../Vue/Menu.php");
--- MVC/PTUT_MVC/params.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "mysql51-101.perso";
-$user = "pommedepweb";
-$password = "azerty123";
-$dbname = "pommedepweb";
-?>
